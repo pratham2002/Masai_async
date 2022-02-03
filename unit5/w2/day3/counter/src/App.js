4beccdb1287be09b337f3dd48de3da8c652cb477
Pratham <agrawalpratham2002@gmail.com>
2022-02-03 21:49:12 +0530

    ..

--- unit5/w2/day3/counter/src/App.js
+++ unit5/w2/day3/counter/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import { Counter } from './Component/Counter';
 
 function App() {
   return (
     <div className="App">
+      <Counter/>
       <Counter/>
     </div>
   );
