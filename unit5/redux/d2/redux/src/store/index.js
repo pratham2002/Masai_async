1f69192ac421466aa9f17be276c750f50b4fcc31
Pratham <agrawalpratham2002@gmail.com>
2022-03-15 11:47:57 +0530

    ..

--- unit5/redux/d2/redux/src/store/index.js
+++ unit5/redux/d2/redux/src/store/index.js
@@ -2,5 +2,4 @@ import { createStore } from "redux";
 import { initialState } from "./initialState";
 import { reducer } from "./reducer";
 import { applyMiddleware } from "redux";
-
 export const store = createStore(reducer, initialState);
